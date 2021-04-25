6446131562821e2c06ce5eb73326ae4804a348ef
Sathish Kumar T.M <sathish.infotech2000@gmail.com>
2021-04-25 22:23:43 +0530

    New Committed..

--- app/src/main/java/com/sathish/code/splashscreen/SecondActivity.java
+++ app/src/main/java/com/sathish/code/splashscreen/SecondActivity.java
@@ -20,7 +20,7 @@ public class SecondActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "New Action played Here..", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
